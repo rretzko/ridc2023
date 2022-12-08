94ecc3a9dff61c77cd0dc1c42e05f8c1c578e6e0
MFR Holdings <rick@mfrholdings.com>
2022-12-08 10:42:44 -0500

    Added table formatting.

--- resources/views/components/tables/students.blade.php
+++ resources/views/components/tables/students.blade.php
@@ -2,46 +2,49 @@
 'students'
 ])
 <x-tables.tables-style />
-<table>
-    <thead>
-    <tr>
-        <td colspan="5" style="text-align: right;border: 0; border-bottom: 1px solid black;" >
-            <a href="{{ route('users.accepteds.students.create') }}" colspan="5"
-               class="text-blue-600 text-sm"
-            >
-                <button class="bg-green-200 text-black px-4 rounded-full">
-                    Add
-                </button>
-            </a>
-        </td>
-    </tr>
-    <tr>
-        <th>###</th>
-        <th>Name</th>
-        <th>Grade</th>
-        <th class="" style="color: transparent;">Edit</th>
-        <th class="" style="border-right: 1px solid black; color: transparent">Remove</th>
-    </tr>
-    </thead>
-    <tbody>
-    @forelse($students AS $student)
+
+<div class="table-container">
+    <table>
+        <thead>
         <tr>
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ $student->fullNameAlpha }}</td>
-            <td class="text-center">{{ $student->grade }}</td>
-            <td>
-                <x-buttons.edit href='/user/students/edit/{{ $student->id }}' />
-            </td>
-            <td>
-                <x-buttons.remove href='/user/students/remove/{{ $student->id }}' />
+            <td colspan="5" style="text-align: right;border: 0; border-bottom: 1px solid black;">
+                <a href="{{ route('users.accepteds.students.create') }}" colspan="5"
+                   class="text-blue-600 text-sm"
+                >
+                    <button class="bg-green-200 text-black px-4 rounded-full">
+                        Add
+                    </button>
+                </a>
             </td>
         </tr>
-    @empty
         <tr>
-            <td colspan="5">
-                No students found
-            </td>
+            <th>###</th>
+            <th>Name</th>
+            <th>Grade</th>
+            <th class="" style="color: transparent;">Edit</th>
+            <th class="" style="border-right: 1px solid black; color: transparent">Remove</th>
         </tr>
-    @endforelse
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+        @forelse($students AS $student)
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $student->fullNameAlpha }}</td>
+                <td class="text-center">{{ $student->grade }}</td>
+                <td>
+                    <x-buttons.edit href='/user/students/edit/{{ $student->id }}'/>
+                </td>
+                <td>
+                    <x-buttons.remove href='/user/students/remove/{{ $student->id }}'/>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="5">
+                    No students found
+                </td>
+            </tr>
+        @endforelse
+        </tbody>
+    </table>
+</div>
--- resources/views/components/tables/tables-style.blade.php
+++ resources/views/components/tables/tables-style.blade.php
@@ -1,4 +1,5 @@
 <style>
-    table{border-collapse: collapse; margin: auto;}
+    table{border-collapse: collapse; margin: 1rem;}
     td,th{border: 1px solid black; padding: 0 0.25rem;}
+    .table-container{background-color: white; padding:0  1rem; padding-bottom: 0.25rem;}
 </style>
